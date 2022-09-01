fde7f7f6b47a223577e3659aaab0a82dffefdba0
Marijo <marijo.torre.03@gmail.com>
2022-08-31 21:07:56 -0500

    solve lab

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/photogallery", (req, res) => {
+    res.sendFile(__dirname + "/views/photogallery.html");
+});
+
+app.listen(3000, () =>{});
